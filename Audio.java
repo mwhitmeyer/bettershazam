fdac3ce02592c004a655273d9cfb2bb226e39028
gillianchu <gillichu@berkeley.edu>
2018-05-26 15:49:28 -0700

    Added Complex.java class so we can create Complex[][] from sample chunks

--- Audio.java
+++ Audio.java
@@ -21,6 +21,7 @@ public class Audio {
     static int frameSize = 4;
     static boolean signed = true;     //Indicates whether the data is signed or unsigned
     static boolean bigEndian = false;  //Indicates whether the audio data is stored in big-endian or little-endian
+    final int[] RANGE = new int[] { 40, 80, 120, 180, 300 };
 
     Hashtable blah = new Hashtable();
 
@@ -111,7 +112,43 @@ public class Audio {
         //Use complex numbers for freq domain
         Complex[][] res = new Complex[sampleChunks][];
 
+        for(int i = 0; i < sampleChunks; i++) {
+            Complex[] complexArray = new Complex[4*1024];
 
+            for(int j = 0; j < chunkSize; j++) {
+                complexArray[i] = new Complex((double) bytes[(i*chunkSize)+j], (double) 0);
+            }
+            //Perform FFT analysis on the chunk:
+            res[i] = Complex.fft(complexArray);
+        }
+
+        // Now create digital fingerprint
+        createHashPrint(res);
+
+
+    }
+
+    // find out in which range is frequency
+    public static int getIndex(int freq) {
+        int i = 0;
+        while (RANGE[i] < freq)
+            i++;
+        return i;
+    }
+
+    public static long createHashPrint(Complex[][] result){
+        for (int i=0; i < result.length; i++) {
+            for (int freq=40; freq < 300; freq++) {
+                // Get the magnitude:
+                double mag = Math.log(result[i][freq].abs() + 1);
+
+                // Find out which range we are in:
+                int index = getIndex(freq);
+
+                // Save the highest magnitude and corresponding frequency:
+
+            }
+        }
 
     }
 

903efd528e83e74ffbfd6a3ed0104b65227966d6
mwhitmeyer <mwhitmeyer@berkeley.edu>
2018-05-27 18:02:44 -0700

    garbage

--- Audio.java
+++ Audio.java
@@ -21,10 +21,10 @@ public class Audio {
     static int frameSize = 4;
     static boolean signed = true;     //Indicates whether the data is signed or unsigned
     static boolean bigEndian = false;  //Indicates whether the audio data is stored in big-endian or little-endian
-    static int[] RANGE = new int[] { 40, 80, 120, 180, 300 };
-
-
-    static Hashtable blah = new Hashtable();
+    static int[] RANGE = new int[] { 40, 80, 120, 180, 400 };
+    static int FUZ_FACTOR = 2;
+    static Hashtable<double[], int[]> allTheFingerprints = new Hashtable<>();
+    
 
 
     public static void main(String[] args){
@@ -137,20 +137,47 @@ public class Audio {
         return i;
     }
 
-    public static long createHashPrint(Complex[][] result){
+    public static double[] createHashPrint(Complex[][] result){
+
+
+        double[] hashes = new double[result.length];
+
+
         for (int i=0; i < result.length; i++) {
-            for (int freq=40; freq < 300; freq++) {
+            double[] highscores = new double[] {0, 0, 0, 0};
+            for (int freq=40; freq < 400; freq++) {
                 // Get the magnitude:
-                double mag = Math.log(result[i][freq].abs() + 1);
+                double mag = Math.log(result[i][freq].abs() + 1); //why is there a log here
 
                 // Find out which range we are in:
                 int index = getIndex(freq);
 
                 // Save the highest magnitude and corresponding frequency:
 
-            }
-        }
+                if (mag > highscores[index]) {
+                    highscores[index] = freq;
 
+                }
+
+            }
+
+            hashes[i] = hash(highscores);
+        }
+        return hashes;
+
+    }
+
+    private long hash(double[] highscores) {
+        double p1 = highscores[0];
+        double p2 = highscores[1];
+        double p3 = highscores[2];
+        double p4 = highscores[3];
+
+
+
+        return (p4 - (p4 % FUZ_FACTOR)) * 100000000 + (p3 - (p3 % FUZ_FACTOR))
+                * 100000 + (p2 - (p2 % FUZ_FACTOR)) * 100
+                + (p1 - (p1 % FUZ_FACTOR));
     }
 
 }
